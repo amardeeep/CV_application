31d681671efd0895061ac99f01b6bd1f4affd441
Deetsy <amardeepdeepak2016@gmail.com>
2024-06-23 15:24:31 +0530

    finish making ui style is to be added

--- cv_application/src/App.jsx
+++ cv_application/src/App.jsx
@@ -29,22 +29,21 @@ function App() {
   const [isSave,setIsSave]=useState(false)
   return(
     <div className='App'>
-      <div className="Editseciton" >
-            <h1>Edit Section :</h1>
-            <div className='Personal'>
+      <div className="editSeciton" >
+            <div className='Personal card'>
               <h2>Personal Details :</h2>
               <Input type="email" name="name" value={formData.name} disabled={isSave} onChange={handleChange}/>        
               <Input type="email" name="email" value={formData.email} disabled={isSave} onChange={handleChange}/>
               <Input type="number" name="phone" value={formData.phone} disabled={isSave} onChange={handleChange}/>
             </div>
-            <div className="EduExperience">
+            <div className="EduExperience card">
               <h2>Education Experience :</h2>
               <Input type="text" name="schoolName" value={formData.schoolName} disabled={isSave} onChange={handleChange} />
               <Input type="text" name="titleOfStudy" value={formData.titleOfStudy} disabled={isSave} onChange={handleChange}/>
               <Input type="date" name='dateJoining' value={formData.dateJoining} disabled={isSave} onChange={handleChange}/>
               <Input type="number" name="duration" value={formData.duration} disabled={isSave} onChange={handleChange}/>
             </div>
-            <div className="WorkExperience">
+            <div className="WorkExperience card">
               <h2>Work Experience :</h2>
               <Input type="text" name="companyName" value={formData.companyName} disabled={isSave} onChange={handleChange}/>
               <Input type="text" name="positionTitle" value={formData.positionTitle} disabled={isSave} onChange={handleChange}/>
@@ -53,16 +52,16 @@ function App() {
               <Input type="checkbox" name="checkbox"  value={formData.checkbox} disabled={isSave} onChange={handleChange}/>
               <Input type="text" name="responsibilities" value={formData.responsibilities} disabled={isSave} onChange={handleChange}/>
             </div>
-            <div className='buttons'>
+            <div className='buttons card'>
               <button onClick={()=>{setIsSave(false)}}>Edit</button>
               <button onClick={()=>{setIsSave(true)}}>Submit</button>
             </div>
           </div>
-        <div className="DisplaySection">
-        {(isSave)&&(
+        <div className="displaySection">
+        {(isSave)?(
           <div>
             <div className='PersonalD'>
-              <h1>Personal Details :</h1>
+              <h2>Personal Details :</h2>
               <div>
                 <h2>{formData.name}</h2>
                 <h3>{formData.email}</h3>
@@ -70,7 +69,7 @@ function App() {
               </div>
             </div>
             <div className='EduD'>
-                <h1>Education Details :</h1>
+                <h2>Education Details :</h2>
             <div>
                 <h3>{formData.schoolName}</h3>
                 <h3>{formData.titleOfStudy}</h3>
@@ -79,7 +78,7 @@ function App() {
             </div>
             </div>
             <div className="WorkD">
-            <h1>Work Experience</h1>
+            <h2>Work Experience :</h2>
             <div>
                 <h3>{formData.companyName}</h3>
                 <h3>{formData.positionTitle} </h3>
@@ -88,7 +87,7 @@ function App() {
                 <h3>{formData.responsibilities} </h3>
             </div>
         </div>
-        </div>)}
+        </div>):(<h3>Press submit to preview your resume!</h3>)}
             
         </div>
     </div>
