4072de7e019195128b27cb7c1cb460bacf72a49a
Deetsy <amardeepdeepak2016@gmail.com>
2024-06-23 15:45:23 +0530

    change structure

--- cv_application/src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-function Input(props){
-    return (
-        <div className="Input">
-            <label htmlFor="">{(props.name).toUpperCase()} : </label>
-            <input type={props.type} name={props.name} value={props.value} disabled={props.disabled?true:false} onChange={props.onChange} />
-        </div>
-    )
-}
-export default Input
